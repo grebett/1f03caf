250ac938c65285a6107e985485d0167f80882c2e
Grégory Betton <gregory.betton@gmail.com>
2017-03-31 14:21:57 +0200

    fix: non blocking errors as some are warnings

--- email_sender/v0.1/render-mjml.js
+++ email_sender/v0.1/render-mjml.js
@@ -1,3 +1,5 @@
+const chalk = require('chalk');
+
 module.exports = function (mjml) {
   const MjmlClient = require('./services/mjml-api');
   const client = new MjmlClient(process.env.MJML_APPLICATION_ID, process.env.MJML_SECRET_KEY);
@@ -5,8 +7,10 @@ module.exports = function (mjml) {
   return new Promise((resolve, reject) => {
     client.render(mjml).then(data => {
       if (data.errors.length) {
-        data.errors.message = 'Errors in your MJML:';
-        reject(data.errors);
+        // some errors are warnings and shoudn't be blocking... we resolve the html anyway
+        // could change if the API distinguishes errors from warnings
+        console.error(chalk.yellow('Errors/Warnings in your MJML'), data.errors);
+        resolve(data.html);
       }
       resolve(data.html);
     }, clientError => {
